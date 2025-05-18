95a6bef9256e68b9d8c09ed78e9137c96b342047
Arthur Lincoln <arthurlincolndev@gmail.com>
2025-05-18 19:12:28 -0300

    commit eaterycard

---
+++ app/src/main/java/com/cornelappdev/eaterycard/ui/theme/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.cornelappdev.eaterycard.ui.theme
+
+import androidx.compose.runtime.Composable
+import com.cornelappdev.eaterycard.EateryCard
+
+@Composable
+fun MainScreen() {
+    EateryCard()
+}
